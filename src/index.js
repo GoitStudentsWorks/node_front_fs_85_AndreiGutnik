4e29162ffbf7a2c74e3ad83f3ec3c2486d562f77
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-23 14:47:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="WaterTracker_Frontend">
+        <BrowserRouter basename="node_front_fs_85_AndreiGutnik">
           <HelmetProvider>
             <ModalProvider>
               <App />
